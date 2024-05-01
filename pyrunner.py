4c054226e916e2d74c64481a152476ccf5467bf9
Hang <64747715+xx529@users.noreply.github.com>
2024-05-01 22:21:18 +0800

    修改代码执行器

--- pyrunner.py
+++ pyrunner.py
@@ -11,7 +11,7 @@ from IPython.utils.capture import capture_output
 from loguru import logger
 
 
-class PythonRunner:
+class InteractivePythonRunner:
     ANSI_ESCAPE = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
     ERROR_DELIMITERS = '-' * 75
 
@@ -43,49 +43,43 @@ class PythonRunner:
         return self
 
 
-# class CondaEnv:
-#     BASE_CONDA_ENV_PATH = '/Users/lzx/miniconda3/envs/runner'
-#     BASE_CONDA_ENV_NAME = 'runner'
+class PythonRunner:
 
-#     def __init__(self, env_name: str = None):
-#         self.env_name = env_name or uuid.uuid4().hex
-#         self.py_console = None
-#         self.py_runner: PythonRunner = None
+    def __init__(self, interpreter_path: Path):
+        self.interpreter_path = interpreter_path
 
-#     def run_script(self, script: Path) -> CompletedProcess:
-#         return self.py_console.run_script(script)
-
-#     def __enter__(self):
-#         logger.info(f'create new conda env: {self.env_name}')
-#         args = ['conda', 'create', '--name', self.env_name, '--clone', self.BASE_CONDA_ENV_NAME]
-#         result = subprocess.run(args, capture_output=True, text=True)
-#         logger.info(result.stdout)
-
-#         args = ['conda', 'run', '--name', self.env_name, 'which', 'python']
-#         python_path = subprocess.run(args, capture_output=True, text=True).stdout.strip()
-#         logger.info(f'python_path: {python_path}')
-#         self.py_runner = PythonRunner(interpreter=Path(python_path))
-#         self.py_console = partial(PythonRunner, interpreter=Path(python_path))
-#         return self
-
-#     def __exit__(self, exc_type, exc_val, exc_tb):
-#         logger.info(f'remove conda env: {self.env_name}')
-#         args = ['conda', 'env', 'remove', '--name', self.env_name]
-#         subprocess.run(args, capture_output=True, text=True)
-#         return self
+    def run_script(self, script: Path) -> CompletedProcess:
+        args = [str(self.interpreter_path), str(script)]
+        return subprocess.run(args, capture_output=True, text=True)
 
 
-ipython_idr = Path('/Users/lzx/miniconda3/envs/runner/bin')
-with PythonRunner(ipython_idr) as r:
-    o, e = r.run('print(1)')
-    print(o, e)
-    print('-----------------')
-    o, e = r.run('import pandas as pd')
-    print(o, e)
-    print('-----------------')
-    o, e = r.run('print("test")')
-    print(o, e)
-    o, e = r.run('print(x)')
-    print(o, e)
-    o, e = r.run('print("aaaaaaaaa")')
-    print(o, e)
+class CondaEnv:
+    BASE_CONDA_ENV_PATH = '/Users/lzx/miniconda3/envs/runner'
+    BASE_CONDA_ENV_NAME = 'runner'
+
+    def __init__(self, env_name: str = None):
+        self.env_name = env_name or uuid.uuid4().hex
+        self.py_console: InteractivePythonRunner = None
+        self.py_runner: PythonRunner = None
+
+    def run_script(self, script: Path) -> CompletedProcess:
+        return self.py_runner.run_script(script)
+
+    def __enter__(self):
+        logger.info(f'create new conda env: {self.env_name}')
+        args = ['conda', 'create', '--name', self.env_name, '--clone', self.BASE_CONDA_ENV_NAME]
+        result = subprocess.run(args, capture_output=True, text=True)
+        logger.info(result.stdout)
+
+        args = ['conda', 'run', '--name', self.env_name, 'which', 'python']
+        python_path = subprocess.run(args, capture_output=True, text=True).stdout.strip()
+        logger.info(f'python_path: {python_path}')
+        self.py_runner = PythonRunner(interpreter_path=Path(python_path))
+        self.py_console = partial(InteractivePythonRunner, interpreter=Path(python_path).parent)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        logger.info(f'remove conda env: {self.env_name}')
+        args = ['conda', 'env', 'remove', '--name', self.env_name]
+        subprocess.run(args, capture_output=True, text=True)
+        return self
